f8b0cc99b0a1bdabb87307014e564bfc5d78f29b
Spartanhalo007 <juanesreyesrangel@gmail.com>
2024-03-09 00:14:33 -0500

    creacion proyecto

---
+++ app/src/main/java/com/example/groupprojectcalculator/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.groupprojectcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.lang.Math.*
+
+class MainActivity : AppCompatActivity() {
+
+
+}
